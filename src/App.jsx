75e2a1c32360517b22af3dfca2368fee944cd4b1
Megh39 <148150265+webtesting22@users.noreply.github.com>
2024-12-11 17:59:09 +0530

    removed modal and updated favicon

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Modal } from 'antd';
 
 function App() {
-  const [isModalOpen, setIsModalOpen] = useState(true); // Modal opens on page load
-  const handleCancel = () => {
-    setIsModalOpen(false); // Close the modal on "Cancel"
-  };
+  // const [isModalOpen, setIsModalOpen] = useState(true); // Modal opens on page load
+  // const handleCancel = () => {
+  //   setIsModalOpen(false); // Close the modal on "Cancel"
+  // };
 
   return (
     <>
@@ -17,18 +17,17 @@ function App() {
 
       </Router>
       {/* Modal */}
-      <Modal
+      {/* <Modal
         title="Welcome!"
         footer={null}
         open={isModalOpen}
-        // footer={null}
         onCancel={handleCancel}
       >
 
         <p>Our Website is under development. Stay tuned for the full website launch!</p>
         <br/>
         <img src="/Images/Loading.jpg" style={{width:"100%"}}/>
-      </Modal>
+      </Modal> */}
     </>
   )
 }
