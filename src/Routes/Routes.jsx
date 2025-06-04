6f3caf6444f76fc9bc99a6dd8dfee79e69040d95
webtesting22 <148150265+webtesting22@users.noreply.github.com>
2025-06-04 17:51:34 +0530

    Update Routes.jsx

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { useLocation, Routes, Route, Navigate } from 'react-router-dom';
 
-import MegaNavigation from "../Components/MegaNavigation.jsx/MegaNavigation";
+// import MegaNavigation from "../Components/MegaNavigation.jsx/MegaNavigation";
 import HomeRoutes from "../HomeRoutes/HomeRoutes";
-import IndieSemicProduct from "../StoreComponents/ProductPage/indieSemicProduct";
+// import IndieSemicProduct from "../StoreComponents/ProductPage/indieSemicProduct";
 import Product from "../StoreComponents/ProductPage/Product";
 import Footer from "../Components/Footer/Footer"
 import Modules from "../Components/Modules/Modules";
 import SOCModule from "../Components/SOCModule/SOCModule";
-import SeparateProductPage from "../StoreComponents/ProductPage/SeparateProductpage";
+// import SeparateProductPage from "../StoreComponents/ProductPage/SeparateProductpage";
 import IndividualProduct from "../StoreComponents/ProductPage/individualProduct";
 import NavigationWrap from "../Components/MegaNavigation.jsx/MegaNavigatioIndex";
 import Services from "../Components/Services/Services";
