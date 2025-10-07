7caacc5fc192fd345bd82c1f327e9d6fce62f992
Neel Patel <webtesting2232@gmail.com>
2025-10-07 08:41:24 +0530

    fix: nothing

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['8ca2-2401-4900-8899-9604-7c4c-7f78-fa67-325d.ngrok-free.app'],
+    allowedHosts: [
+      "8ca2-2401-4900-8899-9604-7c4c-7f78-fa67-325d.ngrok-free.app",
+    ],
+    port: 3002,
   },
-})
+});
